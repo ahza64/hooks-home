d6466c4fdc37f342afe5e228c6eddcded94517cf
ahza64 <justinmichaelpettit@gmail.com>
2021-10-05 16:13:51 -0700

    query with hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { useState } from 'react'
 
 function App() {
+  const [userQuery, setUserQuery] = useState('');
+
+  const updateUserQuery = event => {
+    setUserQuery(event.target.value);
+    console.log('userQuery', userQuery);
+  }
+
+  const handleKepPress = event => {
+    if (event.key === 'Enter') {
+      searchQuery();
+    }
+  }
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank');
+  }
+
   return (
     <div className="App">
-      App
+      <h1>Hello Badass</h1>
+      <div className='form'>
+        <input type="text" value={userQuery} onChange={updateUserQuery} onKeyPress={handleKepPress} />
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
