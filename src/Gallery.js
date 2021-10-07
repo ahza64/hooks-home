c36252ee581af2df9217ab52c8947229e1eeeed9
ahza64 <justinmichaelpettit@gmail.com>
2021-10-07 14:16:35 -0700

    seed data and interval timer useEffect Gallery component

---
+++ src/Gallery.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import PICTURES from './data/pictures'
+
+const Gallery = () => {
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => {
+      setIndex(
+        storedIndex => {
+          return (storedIndex+1)%PICTURES.length
+        }
+      )
+    }, 3000);
+  }, [])
+
+  console.log('index', index);
+
+  return (
+    <div className='Gallery'>
+      <img src={PICTURES[index].image} alt="gallery"/>
+    </div>
+  )
+}
+
+export default Gallery
